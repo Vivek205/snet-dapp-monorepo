7f57da3fc376f710050777a45543efe4c60752f4
Vivek205 <n.vin95@gmail.com>
2019-11-28 16:57:21 +0530

    updated relative imports

--- packages/shared/dist/components/SnetSvgLogo/index.js
+++ packages/shared/dist/components/SnetSvgLogo/index.js
@@ -7,7 +7,7 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _BlackLogo = _interopRequireDefault(require("../../../assets/images/BlackLogo.svg"));
+var _BlackLogo = _interopRequireDefault(require("../../assets/images/BlackLogo.svg"));
 
 require("./styles.css");
 
--- packages/shared/src/components/SnetSvgLogo/index.js
+++ packages/shared/src/components/SnetSvgLogo/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Logo from "../../../assets/images/BlackLogo.svg";
+import Logo from "../../assets/images/BlackLogo.svg";
 import "./styles.css";
 
 const SnetSvgLogo = () => {
