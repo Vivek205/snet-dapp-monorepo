19f0618e981018bafe2d17a62f1049dabbb38c8a
Vivek205 <n.vin95@gmail.com>
2019-11-29 18:42:28 +0530

    migrated RatingsCount

--- packages/marketplace-dapp/src/components/AiMarketplace/MainSection/ServiceCollection/CardGroup/GridViewItem/index.js
+++ packages/marketplace-dapp/src/components/AiMarketplace/MainSection/ServiceCollection/CardGroup/GridViewItem/index.js
@@ -11,7 +11,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Avatar from "@material-ui/core/Avatar";
 import truncate from "lodash/truncate";
 
-import RatingsCount from "../../../../../common/RatingsCount";
+import RatingsCount from "shared/dist/components/RatingsCount";
 import CardImg from "../../../../../../assets/images/SnetDefaultServiceImage.png";
 import SingularityLogo from "../../../../../../assets/images/avatar.png";
 import { useStyles } from "./styles";
