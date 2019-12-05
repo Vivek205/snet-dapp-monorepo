4a8ef7e5f1b8c73d5853ec4ff95f513e08c698ab
dasari-ananya <ananya.dasari@nirman.io>
2019-12-05 18:40:12 +0530

    enroll page design with responsive

--- packages/publisher-dapp/src/Assets/MUITheme.js
+++ packages/publisher-dapp/src/Assets/MUITheme.js
@@ -1,11 +1,76 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const MUITheme = createMuiTheme({
-  palette: {
-    background: {
-      footer: "#211D24",
+  breakpoints: {
+    keys: ["xs", "sm", "md", "lg", "xl"],
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
     },
   },
+  palette: {
+  	text:{
+  		primary: '#666',
+  		secondary: '#fff',
+  		disabled: '#D6D6D6',
+  		red: "#D0021B",
+      darkGray: '#212121',
+      lightGray: '#9b9b9b',
+  		hover:{
+  			blue: "#005ACB",
+  			red: "#D0021B",
+  			black: '#333',
+  		},
+  	},
+  	primary:{
+  		main: '#4086ff',
+  	},
+    background: {
+    	disabled:{
+    		gray: '#D6D6D6',
+    	},
+    	hover:{
+    		blue: 'rgba(0,90,203,0.05)',
+    		transparentBlue: 'rgba(64,134,255,0.1)',
+    		red: "#D0021B",
+    		black: '#333',
+  		},
+    	mainContent: '#fafafa',
+      footer: "#211D24",
+      white: '#fff',
+      black: '#333',
+      red: "#D0021B"
+    },
+    checkCircle: '#00C48C'
+  },
+  typography:{
+  	fontFamily: 'Muli',
+  	h2:{
+  		color: 'rgba(0,0,0,0.87)',
+  		fontSize: 36,
+  		fontWeight: 600,
+  		lineHeight: '45px'
+  	},
+    h3:{
+      color: 'rgba(0,0,0,0.87)',
+      fontSize: 24,
+      fontWeight: 600,
+      lineHeight: '30px'
+    },
+  	body1:{
+  		color: '#616161',
+  		fontSize: 18,
+  		lineHeight: '28px'
+  	},
+    subtitle1:{
+      color: '#9b9b9b',
+      fontSize: 24,
+      lineHeight: '30px'
+    }
+  }
 });
 
 export default MUITheme;
--- packages/shared/dist/components/StyledButton/styles.js
+++ packages/shared/dist/components/StyledButton/styles.js
@@ -14,76 +14,75 @@ var useStyles = (0, _styles.makeStyles)(function (theme) {
       borderStyle: "solid",
       borderColor: "transparent",
       padding: "13px 28px 11px",
-      color: theme.palette.text.white,
+      color: theme.palette.text.secondary,
       textTransform: "uppercase",
-      fontFamily: theme.typography.primary.main,
       fontWeight: 600,
       letterSpacing: "1.25px",
       lineHeight: "16px",
       "&:disabled": {
-        backgroundColor: theme.palette.text.lightGray,
-        color: theme.palette.text.white
+        backgroundColor: theme.palette.background.disabled.gray,
+        color: theme.palette.text.secondary
       }
     },
     blueBg: {
-      backgroundColor: theme.backgroundColor.blue,
+      backgroundColor: theme.palette.primary.main,
       "&:hover": {
-        backgroundColor: theme.palette.text.customHoverBlue
+        backgroundColor: theme.palette.text.hover.blue
       }
     },
     blackBg: {
-      backgroundColor: theme.backgroundColor.githubBlack,
+      backgroundColor: theme.palette.background.black,
       "& i": {
         fontSize: 24,
         marginRight: 5
       },
       "&:hover": {
-        backgroundColor: theme.backgroundColor.white,
-        borderColor: theme.backgroundColor.githubBlack,
-        color: theme.backgroundColor.githubBlack
+        backgroundColor: theme.palette.background.white,
+        borderColor: theme.palette.background.black,
+        color: theme.palette.text.hover.black
       }
     },
     transparentBg: {
       backgroundColor: "transparent",
       color: theme.palette.text.primary,
       "&:disabled": {
-        color: "#ccc !important",
+        color: "".concat(theme.palette.text.disabled, " !important"),
         backgroundColor: "transparent"
       },
       "&:hover": {
-        backgroundColor: "rgba(64,134,255,0.1)",
-        color: theme.palette.text.customHoverBlue
+        backgroundColor: theme.palette.background.hover.transparentBlue,
+        color: theme.palette.text.hover.blue
       }
     },
     red: {
-      color: theme.palette.text.redBtnText,
+      color: theme.palette.text.red,
       "&:hover": {
-        backgroundColor: theme.palette.text.redBtnText,
-        color: theme.palette.text.white
+        backgroundColor: theme.palette.background.hover.red,
+        color: theme.palette.text.secondary
       }
     },
     redBg: {
       padding: "13px 38px 11px",
-      color: theme.palette.text.white,
-      backgroundColor: theme.palette.text.redBtnBg,
+      color: theme.palette.text.secondary,
+      backgroundColor: theme.palette.background.red,
       "&:hover": {
-        backgroundColor: theme.palette.text.redBtnText,
-        color: theme.palette.text.white
+        backgroundColor: theme.palette.background.hover.red,
+        color: theme.palette.text.secondary
       }
     },
     transparentBlueBorder: {
-      borderColor: theme.palette.text.primary,
+      borderColor: theme.palette.primary.main,
       backgroundColor: "transparent !important",
-      color: theme.palette.text.primary,
+      color: theme.palette.primary.main,
       "&:hover": {
-        borderColor: theme.palette.text.transBlueBorderTextHover,
-        backgroundColor: "rgba(0,90,203,0.05) !important",
-        color: theme.palette.text.transBlueBorderTextHover
+        borderColor: theme.palette.text.hover.blue,
+        backgroundColor: "".concat(theme.palette.background.hover.transparentBlue, " !important"),
+        color: theme.palette.text.hover.blue
       },
       "&:disabled": {
         borderWidth: 2,
-        borderColor: theme.palette.text.disabledBtnBg,
-        color: theme.palette.text.disabledBtnBg
+        borderColor: theme.palette.background.disabled,
+        color: theme.palette.text.disabled
       }
     }
   };
