eec9cd427e8f2e3af9d3341f801e329576d1eefe
Vivek205 <n.vin95@gmail.com>
2019-12-12 18:46:52 +0530

    added dispatch for user login

--- packages/publisher-dapp/src/Pages/Login/index.js
+++ packages/publisher-dapp/src/Pages/Login/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import SNETLogin from 'shared/dist/components/SNETLogin';
+import SNETLogin from "shared/dist/components/SNETLogin";
+import { userActions } from "../../Services/Redux/actionCreators";
+import { loginErrorMsg } from "./content";
 
 const Login = () => {
-    return (
-        <SNETLogin  />
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(undefined);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = async () => {
+    try {
+      await dispatch(userActions.login(email, password));
+    } catch (error) {
+      setError(loginErrorMsg);
+    }
+  };
+
+  return (
+    <SNETLogin
+      title="Welcome back"
+      email={email}
+      onEmailChange={e => setEmail(e.target.value)}
+      password={password}
+      onPasswordChange={e => setPassword(e.target.value)}
+      //   TODO : create a page for forgotPassword and pass the link here
+      //   forgotPasswordLink={}
+      loginError={error}
+      handleSubmit={handleSubmit}
+    />
+  );
 };
 
 export default Login;
