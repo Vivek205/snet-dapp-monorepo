bd4adba992519dc5fc34d1a9cb195e862c668056
Vivek205 <n.vin95@gmail.com>
2019-12-17 00:51:28 +0530

    removed unnecessary code

--- packages/publisher-dapp/src/Pages/Onboarding/constant.js
+++ packages/publisher-dapp/src/Pages/Onboarding/constant.js
@@ -1,30 +1,7 @@
-// export const steps = {
-//   ENTITY: "ENTITY",
-//   TNC: "TNC",
-//   AUTHENTICATE: "AUTHENITCATE",
-// };
-
 export const onboardingSections = {
   ENTITY: { key: 1, heading: { title: "entity", description: "entity" } },
   TNC: { key: 2, heading: { title: "terms of use", description: "terms of use" } },
   AUTHENTICATE: { key: 3, heading: { title: "authenticate", description: "authenticate" } },
 };
-
-// export const stepsToKeys = {
-//   ENTITY: 1,
-//   TNC: 2,
-//   AUTHENTICATE: 3,
-// };
-
-// export const keysToSteps = {
-//   1: "ENTITY",
-//   2: "TNC",
-//   3: "AUTHENTICATE",
-// };
-
-// export const stepsLimit = {
-//   FIRST: 1,
-//   LAST: 3,
-// };
 
 export const progressText = ["entity", "terms of use", "authenticate"];
--- packages/publisher-dapp/src/Pages/Onboarding/index.js
+++ packages/publisher-dapp/src/Pages/Onboarding/index.js
@@ -1,84 +1,17 @@
-import React, { useState, Fragment, useEffect } from "react";
-import {
-  // titles,
-  // descriptions,
-  progressText,
-  // stepsToKeys,
-  steps,
-  // stepsLimit,
-  // keysToSteps,
-  onboardingSections,
-} from "./constant";
+import React, { Fragment } from "react";
+import { progressText, onboardingSections } from "./constant";
 import ProgressBar from "shared/dist/components/ProgressBar";
 import { withStyles } from "@material-ui/core/styles";
 
-// import Entity from "./Entity";
-// import TNC from "./TNC";
-// import Authenticate from "./Authenticate";
 import { useStyles } from "./styles";
-// import Navigation from "./Navigation";
 import { OnboardingRoutes } from "./OnboardingRouter/Routes";
 import OnboardingRouter from "./OnboardingRouter";
 import Heading from "./Heading";
 
-const Onboarding = ({ match, classes, history, location }) => {
-  // const [currentStep, setCurrentStep] = useState(steps.ENTITY);
-
-  // useEffect(() => {
-  //   setCurrentStep();
-  // }, []);
-
-  // const onboardingSections = {
-  //   ENTITY: {
-  //     title: titles.ENTITY,
-  //     description: descriptions.ENTITY,
-  //     component: <Entity />,
-  //     path: OnboardingRoutes.ENTITY.path,
-  //   },
-  //   TNC: {
-  //     title: titles.TNC,
-  //     description: descriptions.TNC,
-  //     component: <TNC />,
-  //     path: OnboardingRoutes.TNC.path,
-  //   },
-  //   AUTHENTICATE: {
-  //     title: titles.AUTHENTICATE,
-  //     description: descriptions.AUTHENTICATE,
-  //     component: <Authenticate />,
-  //     path: OnboardingRoutes.AUTHENTICATE.path,
-  //   },
-  // };
-
-  // const activeStep = onboardingSections[currentStep];
-
-  // const handleNext = () => {
-  //   if (stepsToKeys[currentStep] === stepsLimit.LAST) {
-  //     return undefined;
-  //   }
-  //   return () => {
-  //     const nextStep = keysToSteps[stepsToKeys[currentStep] + 1];
-  //     history.push(onboardingSections[nextStep].path);
-  //     setCurrentStep(nextStep);
-  //   };
-  // };
-
-  // const handlePrev = () => {
-  //   if (stepsToKeys[currentStep] === stepsLimit.FIRST) {
-  //     return undefined;
-  //   }
-  //   return () => {
-  //     const prevStep = keysToSteps[stepsToKeys[currentStep] - 1];
-  //     history.push(onboardingSections[prevStep].path);
-  //     setCurrentStep(prevStep);
-  //   };
-  // };
-
+const Onboarding = ({ location }) => {
   const activeSection = () => {
     const { pathname: path } = location;
-    // stepsToKeys[currentStep]
-
     const strippedPath = path => path.split("/")[2];
-
     const { ENTITY, TNC, AUTHENTICATE } = onboardingSections;
 
     switch (strippedPath(path)) {
@@ -102,7 +35,6 @@ const Onboarding = ({ match, classes, history, location }) => {
       <Heading {...activeSection().heading} />
       <ProgressBar activeSection={activeSection()} progressText={progressText} />
       <OnboardingRouter />
-      {/* <Navigation handleNext={handleNext()} handlePrev={handlePrev()} /> */}
     </Fragment>
   );
 };
