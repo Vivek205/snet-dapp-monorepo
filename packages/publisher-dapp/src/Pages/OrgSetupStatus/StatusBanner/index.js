24a4f9c629fb827ab7721b0206aaef224b557dca
dasari-ananya <ananya.dasari@nirman.io>
2019-12-18 11:14:55 +0530

    data correction

--- packages/publisher-dapp/src/Pages/OrgSetupStatus/StatusBanner/index.js
+++ packages/publisher-dapp/src/Pages/OrgSetupStatus/StatusBanner/index.js
@@ -6,18 +6,17 @@ import { withStyles } from "@material-ui/styles";
 import SNETButton from "shared/dist/components/SNETButton";
 import { useStyles } from "./styles";
 
-const StatusBanner = ({ classes, title, description, CTA }) => {
+const StatusBanner = ({ classes }) => {
   return (
     <Grid container spacing={24} className={classes.statusBannerContainer}>
       <Grid item xs={12} sm={3} md={3} lg={3} className={classes.statusBannerMedia}>
         <img src="http://placehold.it/302x242" alt="Media" />
       </Grid>
       <Grid item xs={12} sm={9} md={9} lg={9} className={classes.statusBannerContent}>
-        <Typography variant="h6">{title}</Typography>
-        <Typography>{description}</Typography>
-        {CTA.map((item, index) => (
-          <SNETButton children={item.label} variant={item.variant} color={item.color} />
-        ))}        
+        <Typography variant="h6">Congratulations! Individual organization entity is approved.</Typography>
+        <Typography>You can continue finishing setting up your company details and publish your company entity to the blockchain.  Then you will be ready to create and publish your new AI services to the AI Marketplace.  You can also invite team members to help setup and manage your AI services more efficiently.</Typography>
+        <SNETButton children="organization setup" variant="contained" color="primary" />
+        <SNETButton children="Invite Team Members" variant="outlined" color="primary" />
       </Grid>
     </Grid>
   );
--- packages/publisher-dapp/src/Pages/OrgSetupStatus/index.js
+++ packages/publisher-dapp/src/Pages/OrgSetupStatus/index.js
@@ -18,14 +18,7 @@ const OrgSetupStatus = ({ classes }) => {
           <Typography variant="h5">Welcome to the AI Publisher</Typography>
           <Typography>With this pubilsher portal, you can publish and manage yourAI services. You will be able to edit your services, demos, and tutorial content.</Typography>
         </Grid>
-        <StatusBanner 
-          title="Congratulations! Individual organization entity is approved."
-          description="You can continue finishing setting up your company details and publish your company entity to the blockchain.  Then you will be ready to create and publish your new AI services to the AI Marketplace.  You can also invite team members to help setup and manage your AI services more efficiently."
-          CTA={[
-            {label:"organization setup" , color: "primary", variant:"contained" },
-            {label: "Invite Team Members" , color: "primary", variant:"outlined" }
-          ]}
-        />
+        <StatusBanner />
         <RelatedLinks />
       </Grid>
     </Grid>    
