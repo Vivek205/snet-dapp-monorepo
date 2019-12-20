e7d12ba2db2489f9a6812ad1f0b28ffc403692fc
Vivek205 <n.vin95@gmail.com>
2019-12-20 15:49:19 +0530

    initializing application

--- packages/publisher-dapp/src/Services/Redux/reducers/userReducer.js
+++ packages/publisher-dapp/src/Services/Redux/reducers/userReducer.js
@@ -2,11 +2,12 @@ import { userActions } from "../actionCreators";
 import { verificationStatuses } from "../../../Pages/Onboarding/constant";
 
 const initialState = {
+  isInitialized: false,
   isLoggedIn: false,
   email: undefined,
   nickname: undefined,
   isEmailVerified: false,
-  verificationStatus: verificationStatuses.NOT_STARTED
+  verificationStatus: verificationStatuses.NOT_STARTED,
 };
 
 const userReducer = (state = initialState, action) => {
@@ -22,6 +23,9 @@ const userReducer = (state = initialState, action) => {
     }
     case userActions.loginActions.SET_USER_EMAIL_VERIFIED: {
       return { ...state, isEmailVerified: action.payload };
+    }
+    case userActions.loginActions.SET_APP_INITIALIZED: {
+      return { ...state, isInitialized: action.payload };
     }
     default:
       return state;
