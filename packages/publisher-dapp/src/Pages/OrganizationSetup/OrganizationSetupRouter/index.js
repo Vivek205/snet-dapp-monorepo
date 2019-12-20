0ddf7120e9871c7ca5fbd46b8fe43e4f3b414d4a
Vivek205 <n.vin95@gmail.com>
2019-12-20 16:34:29 +0530

    added exact to default route

--- packages/publisher-dapp/src/Pages/OrganizationSetup/OrganizationSetupRouter/index.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/OrganizationSetupRouter/index.js
@@ -5,8 +5,8 @@ import { OrganizationSetupRoutes } from "./Routes";
 const OrganizationSetupRouter = () => {
   return (
     <Switch>
-      {Object.values(OrganizationSetupRoutes).map(({ name, path, component }) => (
-        <Route key={name} path={path} component={component} />
+      {Object.values(OrganizationSetupRoutes).map(({ name, path, component, exact }) => (
+        <Route key={name} path={path} component={component} exact={exact}/>
       ))}
       <Route path={OrganizationSetupRoutes.DEFAULT_PAGE.path} component={OrganizationSetupRoutes.DEFAULT_PAGE.component} />
     </Switch>
