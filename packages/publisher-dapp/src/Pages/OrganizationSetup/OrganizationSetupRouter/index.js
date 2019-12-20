2d7ef57da78add668c1e38bca55173360737f756
Vivek205 <n.vin95@gmail.com>
2019-12-20 16:26:47 +0530

    OrganizationSetupRouter default page

--- packages/publisher-dapp/src/Pages/OrganizationSetup/OrganizationSetupRouter/index.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/OrganizationSetupRouter/index.js
@@ -8,6 +8,7 @@ const OrganizationSetupRouter = () => {
       {Object.values(OrganizationSetupRoutes).map(({ name, path, component }) => (
         <Route key={name} path={path} component={component} />
       ))}
+      <Route path={OrganizationSetupRoutes.DEFAULT_PAGE.path} component={OrganizationSetupRoutes.DEFAULT_PAGE.component} />
     </Switch>
   );
 };
