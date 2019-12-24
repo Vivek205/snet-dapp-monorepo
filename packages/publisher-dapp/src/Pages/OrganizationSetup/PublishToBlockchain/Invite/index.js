9081af2a49e481844603b70bc76ee48da023a703
dasari-ananya <ananya.dasari@nirman.io>
2019-12-24 14:15:31 +0530

    styling of publish tab

--- packages/publisher-dapp/src/Pages/OrganizationSetup/PublishToBlockchain/Invite/index.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/PublishToBlockchain/Invite/index.js
@@ -1,33 +1,41 @@
 import React, { Fragment } from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-
+import { withStyles } from "@material-ui/core/styles";
 
 import { useStyles } from "./styles";
 import AlertBox from "shared/dist/components/AlertBox";
 import SNETButton from "shared/dist/components/SNETButton";
 import SNETTextfield from "shared/dist/components/SNETTextfield";
 
-const Invite = () => {
-  const classes = useStyles();
+const Invite = ({ classes }) => {
   return (
     <div className={classes.inviteContainer}>
       <Typography variant="subtitle1">Team Members Accepted Invite</Typography>
       <Typography variant="subtitle2">People you invited and that have accepted will be published to your company’s blockchain.  Any invitations that are currently pending can be added to blockchain by you (the owner) afterwards.</Typography>
       <Grid container className={classes.table}>
         <Grid item xs={12} sm={12} md={12} lg={12} className={classes.tableColumn}>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography>email</Typography></Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography>name</Typography></Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography>status</Typography></Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}><Typography className={classes.th}>email</Typography></Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}><Typography className={classes.th}>name</Typography></Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}><Typography className={classes.th}>status</Typography></Grid>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={12} className={classes.tableData}>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography>johndoe@email.com</Typography></Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography>John Doe</Typography></Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography>Invitation Accepted</Typography></Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
+            <Typography className={classes.mobileTH}>email:</Typography>
+            <Typography className={classes.td}>johndoe@email.com</Typography>            
+          </Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
+            <Typography className={classes.mobileTH}>name:</Typography>
+            <Typography className={classes.td}>John Doe</Typography>
+          </Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
+            <Typography className={classes.mobileTH}>status:</Typography>
+            <Typography className={classes.td}>Invitation Accepted</Typography>
+          </Grid>
         </Grid>
       </Grid>
     </div>
   );
 };
 
-export default Invite;
+export default withStyles(useStyles)(Invite);
--- packages/publisher-dapp/src/Pages/OrganizationSetup/PublishToBlockchain/styles.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/PublishToBlockchain/styles.js
@@ -1,6 +1,31 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export const useStyles = makeStyles(MUITheme => ({
+export const useStyles = MUITheme => ({
+  box:{
+    width: 845,
+    padding: '0 0 37px',
+    borderRadius: 4,
+    margin: '48px auto',
+    backgroundColor: MUITheme.palette.background.white,
+    boxShadow: '0 1px 1px 0 rgba(0,0,0,0.14), 0 2px 1px -1px rgba(0,0,0,0.14), 0 1px 3px 0 rgba(0,0,0,0.2)',
+    '& h6':{
+      padding: '13px 24px',
+      borderBottomWidth: 1,
+      borderBottomStyle: 'solid',
+      borderBottomColor: MUITheme.palette.border.primary
+    },
+    '& + p':{
+      width: 754,
+      margin: '0 auto',
+      [MUITheme.breakpoints.down('sm')]:{ width: 'auto' }
+    },
+    [MUITheme.breakpoints.down('sm')]:{ width: 'auto' }
+  },
+  inputFields:{ padding: '0 24px' },
+  description:{
+    padding: '16px 24px 24px',
+    color: MUITheme.palette.text.primary,
+    fontSize: 14,
+    lineHeight: '21px'
+  },
   buttonsContainer: {
     marginTop: 40,
     marginBottom: 64,
@@ -11,4 +36,4 @@ export const useStyles = makeStyles(MUITheme => ({
     },
     [MUITheme.breakpoints.down("xs")]: { flexDirection: "column" },
   },
-}));
+});
