3c0da6f6066629f49f74b6fd1fcf8569d7fa976c
Vivek205 <n.vin95@gmail.com>
2019-12-26 14:34:17 +0530

    removed unused imports

--- packages/publisher-dapp/src/Pages/OrganizationSetup/PublishToBlockchain/Invite/index.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/PublishToBlockchain/Invite/index.js
@@ -1,28 +1,34 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 
 import { useStyles } from "./styles";
-import AlertBox from "shared/dist/components/AlertBox";
-import SNETButton from "shared/dist/components/SNETButton";
-import SNETTextfield from "shared/dist/components/SNETTextfield";
 
 const Invite = ({ classes }) => {
   return (
     <div className={classes.inviteContainer}>
       <Typography variant="subtitle1">Team Members Accepted Invite</Typography>
-      <Typography variant="subtitle2">People you invited and that have accepted will be published to your company’s blockchain.  Any invitations that are currently pending can be added to blockchain by you (the owner) afterwards.</Typography>
+      <Typography variant="subtitle2">
+        People you invited and that have accepted will be published to your company’s blockchain. Any invitations that
+        are currently pending can be added to blockchain by you (the owner) afterwards.
+      </Typography>
       <Grid container className={classes.table}>
         <Grid item xs={12} sm={12} md={12} lg={12} className={classes.tableColumn}>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography className={classes.th}>email</Typography></Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography className={classes.th}>name</Typography></Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}><Typography className={classes.th}>status</Typography></Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
+            <Typography className={classes.th}>email</Typography>
+          </Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
+            <Typography className={classes.th}>name</Typography>
+          </Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
+            <Typography className={classes.th}>status</Typography>
+          </Grid>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={12} className={classes.tableData}>
           <Grid item xs={12} sm={4} md={4} lg={4}>
             <Typography className={classes.mobileTH}>email:</Typography>
-            <Typography className={classes.td}>johndoe@email.com</Typography>            
+            <Typography className={classes.td}>johndoe@email.com</Typography>
           </Grid>
           <Grid item xs={12} sm={4} md={4} lg={4}>
             <Typography className={classes.mobileTH}>name:</Typography>
