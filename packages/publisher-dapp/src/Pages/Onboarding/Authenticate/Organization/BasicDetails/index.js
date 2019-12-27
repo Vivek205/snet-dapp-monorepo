a70563944bcbec2facb77b8a7dc2018fe1c69e5b
Vivek205 <n.vin95@gmail.com>
2019-12-27 21:53:31 +0530

    moved states of basic details to redux

--- packages/publisher-dapp/src/Pages/Onboarding/Authenticate/Organization/BasicDetails/index.js
+++ packages/publisher-dapp/src/Pages/Onboarding/Authenticate/Organization/BasicDetails/index.js
@@ -2,34 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
 
-import SNETTextField from "shared/dist/components/SNETTextfield"
+import SNETTextField from "shared/dist/components/SNETTextfield";
 import { basicDetailsFormData } from "./content";
+import { useSelector, useDispatch } from "react-redux";
+import { organizationActions } from "../../../../../Services/Redux/actionCreators";
 
 const BasicDetails = props => {
-  const { basicDetails, handleBasicDetailsChange } = props;
+  const { name, website, duns, ownerFullName, phone } = useSelector(state => state.organization);
+  const dispatch = useDispatch();
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    dispatch(organizationActions.setOneBasicDetail(name, value));
+  };
+
   return (
     <Grid container>
-      <SNETTextField
-        {...basicDetailsFormData.COMPANY_NAME}
-        value={basicDetails.name}
-        onChange={handleBasicDetailsChange}
-      />
-      <SNETTextField {...basicDetailsFormData.DUNS} value={basicDetails.duns} onChange={handleBasicDetailsChange} />
-      <SNETTextField
-        {...basicDetailsFormData.WEBSITE}
-        value={basicDetails.website}
-        onChange={handleBasicDetailsChange}
-      />
-      <SNETTextField
-        {...basicDetailsFormData.USER_FULL_NAME}
-        value={basicDetails.ownerFullName}
-        onChange={handleBasicDetailsChange}
-      />
-      <SNETTextField
-        {...basicDetailsFormData.PHONE}
-        value={basicDetails.phone}
-        onChange={handleBasicDetailsChange}
-      />
+      <SNETTextField {...basicDetailsFormData.COMPANY_NAME} value={name} onChange={handleChange} />
+      <SNETTextField {...basicDetailsFormData.DUNS} value={duns} onChange={handleChange} />
+      <SNETTextField {...basicDetailsFormData.WEBSITE} value={website} onChange={handleChange} />
+      <SNETTextField {...basicDetailsFormData.USER_FULL_NAME} value={ownerFullName} onChange={handleChange} />
+      <SNETTextField {...basicDetailsFormData.PHONE} value={phone} onChange={handleChange} />
     </Grid>
   );
 };
