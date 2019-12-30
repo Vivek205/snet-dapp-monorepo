bc5dfd6ed86aee44e0025517da6870003a523c68
Vivek205 <n.vin95@gmail.com>
2019-12-30 11:57:39 +0530

    login banner different content based on the loggedIn state

--- packages/publisher-dapp/src/Pages/Onboarding/Entity/index.js
+++ packages/publisher-dapp/src/Pages/Onboarding/Entity/index.js
@@ -13,6 +13,7 @@ import { OnboardingRoutes } from "../OnboardingRouter/Routes";
 import { userEntities } from "../../../Utils/user";
 import { useDispatch, useSelector } from "react-redux";
 import { onboardingActions } from "../../../Services/Redux/actionCreators/userActions";
+import LoginBanner from "./LoginBanner";
 
 const Entity = ({ classes, history }) => {
   const entity = useSelector(state => state.user.entity);
@@ -45,29 +46,7 @@ const Entity = ({ classes, history }) => {
           onChange={handleEntityChange}
         />
       </Grid>
-      <Grid item sx={12} sm={12} md={12} lg={12} className={classes.box}>
-        <Typography variant="h5">Sign In</Typography>
-        <Grid item sx={12} sm={12} md={12} lg={12} className={classes.signInContent}>
-          <Grid item sx={12} sm={4} md={4} lg={4} className={classes.signInMedia}>
-            <img src="http://placehold.it/150x150" alt="Sign In" />
-          </Grid>
-          <Grid item sx={12} sm={8} md={8} lg={8} className={classes.signInRightContent}>
-            <Typography className={classes.signInSubtitle}>
-              Please Login or Signup into Singularitynet to use the portal.
-            </Typography>
-            <Typography className={classes.signInDescription}>
-              To use the portal, please sign up. Lorem ipsum dolor sit amet, per odio adipi scing ea, est an purto
-              libris fastidii, dolor laboramus consectetuer ut eum. An debet expetendis scriptorem ius. Dolorem detracto
-              accusamus mea cu. Nam hendrerit theophrastus ex, vix aeque solet cu.
-            </Typography>
-            <div className={classes.signInBtns}>
-              <SNETButton color="primary" children="Login to singularitynet" variant="text" />
-              <SNETButton color="primary" children="Create new account" variant="text" />
-            </div>
-          </Grid>
-        </Grid>
-      </Grid>
-
+      <LoginBanner classes={classes} />
       <Grid item sx={12} sm={12} md={12} lg={12} className={classes.box}>
         <Typography variant="h5">Email Preferences</Typography>
         <div className={classes.checkboxContainer}>
