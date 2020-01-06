2821b2dcea82a6462b703ea63270645cd6dad1de
dasari-ananya <ananya.dasari@nirman.io>
2020-01-06 12:18:55 +0530

    loggedIn condition

--- packages/publisher-dapp/src/Pages/Onboarding/SingularityAccount/LoginBanner.js
+++ packages/publisher-dapp/src/Pages/Onboarding/SingularityAccount/LoginBanner.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 
-import { loginBannerDetails } from "./content";
+import { loggedInDetails, loggedOutDetails } from "./content";
 import SNETButton from "shared/src/components/SNETButton";
 import { useSelector } from "react-redux";
 import { GlobalRoutes } from "../../../GlobalRouter/Routes";
@@ -13,20 +13,43 @@ import SingularityLogo from "shared/src/assets/images/avatar.png";
 const LoginBanner = ({ classes, providerName, emailId, orgImg }) => {
   const { isLoggedIn } = useSelector(state => state.user);
 
+  console.log(isLoggedIn)
+
   return (
     <Grid item sx={12} sm={12} md={12} lg={12} className={classes.box}>
-      <Typography variant="h6">Sign in as</Typography>
+      <Typography variant="h6">{isLoggedIn ? 'Sign in as': 'Sign in' }</Typography>
       <Grid item sx={12} sm={12} md={12} lg={12} className={classes.signInContent}>
         <Grid item sx={12} sm={4} md={4} lg={4} className={classes.signInMedia}>
-          <Avatar alt="Singularity" src={orgImg || SingularityLogo} className={classes.avatar} />
-          <div className={classes.userDetails}>
-            <Typography variant="h6">{providerName}</Typography>
-            <Typography variant="subtitle2">{emailId}</Typography>
-          </div>
+        {
+          isLoggedIn ?
+            <Fragment>
+              <Avatar alt="Singularity" src={orgImg || SingularityLogo} className={classes.avatar} />
+              <div className={classes.userDetails}>
+                <Typography variant="h6">{providerName}</Typography>
+                <Typography variant="subtitle2">{emailId}</Typography>
+              </div>
+            </Fragment>
+          :
+            <img src="http://placehold.it/250x150" alt="Login In" />
+        }        
         </Grid>
         <Grid item sx={12} sm={8} md={8} lg={8} className={classes.signInRightContent}>
-          <Typography className={classes.signInSubtitle}>{loginBannerDetails.subtitle}</Typography>
-          <Typography className={classes.signInDescription}>{loginBannerDetails.description}</Typography>
+          <Typography className={classes.signInSubtitle}>
+          {
+            isLoggedIn ?
+              loggedInDetails.subtitle
+            :
+              loggedOutDetails.subtitle
+          }
+          </Typography>
+          <Typography className={classes.signInDescription}>
+          {
+            isLoggedIn ?
+              loggedInDetails.description
+           :
+             loggedOutDetails.description
+          }
+          </Typography>
           <div className={classes.signInBtns}>
             <Link to={GlobalRoutes.LOGIN.path}>
               <SNETButton
--- packages/publisher-dapp/src/Pages/Onboarding/SingularityAccount/content.js
+++ packages/publisher-dapp/src/Pages/Onboarding/SingularityAccount/content.js
@@ -3,9 +3,14 @@ export const entityTypeDetails = {
 	description: 'You will be able to choose publish and developed as Company Organization, Indivdual / Sole Proprietor / Single Person Business or join an existing approved entity with an invitation.  The first two options require certain amount of information to proceed. '
 }
 
-export const loginBannerDetails = {
+export const loggedInDetails = {
 	subtitle: 'This is your Singularlity account that will be associated with your AI Publisher account.',
 	description: 'If you would like to use a different account, you can choose from the options below. If you are an organization, consider registeringa new Singularity account rather using a personal account.'
+}
+
+export const loggedOutDetails = {
+	subtitle: 'Please Login or Signup into Singularitynet to use the portal. ',
+	description: 'To use the portal, please sign up. Lorem ipsum dolor sit amet, per odio adipi scing ea, est an purto libris fastidii, dolor laboramus consectetuer ut eum. An debet expetendis scriptorem ius. Dolorem detracto accusamus mea cu. Nam hendrerit theophrastus ex, vix aeque solet cu.'
 }
 
 export const emailPreferencesList = [
