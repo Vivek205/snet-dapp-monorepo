491e1f7de5fd921e25cbbaf09ad07ddb1374f02e
Vivek205 <n.vin95@gmail.com>
2020-01-29 14:38:01 +0530

    fixed serviceSortOptions

--- packages/publisher-dapp/src/Pages/AiServices/MainSection/ServiceCollection/ToolBar/ServiceSortOptions.js
+++ packages/publisher-dapp/src/Pages/AiServices/MainSection/ServiceCollection/ToolBar/ServiceSortOptions.js
@@ -3,18 +3,18 @@ import React, { Fragment, useState } from "react";
 import StyledDropdown from "shared/dist/components/StyledDropdown";
 import { useStyles } from "./styles";
 
-const ServiceSortOptions = ({ pagination, updatePagination, fetchService }) => {
-  const [activeSortItem, setActiveSortItem] = useState(true);
+const ServiceSortOptions = () => {
+  const [activeSortItem, setActiveSortItem] = useState("");
   const classes = useStyles();
 
-  const handleSortChange = async event => {
-    setActiveSortItem(activeSortItem(false));
+  const handleSortChange = event => {
+    setActiveSortItem(event.target.value);
   };
 
   return (
     <Fragment>
       <span className={classes.sortbyTxt}>Sort by:</span>
-      <StyledDropdown value="value" labelTxt="select" onChange={handleSortChange} />
+      <StyledDropdown value={activeSortItem} labelTxt="select" onChange={handleSortChange} />
     </Fragment>
   );
 };
