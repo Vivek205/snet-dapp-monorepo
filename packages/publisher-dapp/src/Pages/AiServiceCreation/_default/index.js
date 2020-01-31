780e189230d490b2d04be0997e510e7923d64b44
dasari-ananya <ananya.dasari@nirman.io>
2020-01-31 11:08:17 +0530

    removed Verification status

--- packages/publisher-dapp/src/Pages/AiServiceCreation/_default/index.js
+++ packages/publisher-dapp/src/Pages/AiServiceCreation/_default/index.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { ServiceCreationRoutes } from "../ServiceCreationRouter/Routes";
 
 const Default = ({ history }) => {
-  const { verificationStatus } = useSelector(state => state.user);
-
   useEffect(() => {
     return history.push(ServiceCreationRoutes.PROFILE.path);
-  }, [history, verificationStatus]);
+  }, [history]);
 
   return <div />;
 };
