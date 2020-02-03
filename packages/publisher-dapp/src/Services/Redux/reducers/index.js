6293ff8dc20f2a4e44c10e8327f93bb4fd331ae0
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2020-02-03 17:02:12 +0530

    Service Creation Init

--- packages/publisher-dapp/src/Services/Redux/reducers/index.js
+++ packages/publisher-dapp/src/Services/Redux/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import OrganizationReducer from "./organizationReducer";
 import loaderReducer from "./loaderReducer";
+import serviceDetailsReducer from "./serviceDetailsReducers";
 
 const rootReducer = combineReducers({
   user: userReducer,
   organization: OrganizationReducer,
   loader: loaderReducer,
+  service: serviceDetailsReducer,
 });
 
 export default rootReducer;
