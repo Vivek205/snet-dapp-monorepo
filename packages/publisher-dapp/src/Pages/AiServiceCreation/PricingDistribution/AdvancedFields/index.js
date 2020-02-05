82bfa7dda1c82e8d8450148bc5538938deb60182
Vivek205 <n.vin95@gmail.com>
2020-02-05 15:13:17 +0530

    mpeAddress

--- packages/publisher-dapp/src/Pages/AiServiceCreation/PricingDistribution/AdvancedFields/index.js
+++ packages/publisher-dapp/src/Pages/AiServiceCreation/PricingDistribution/AdvancedFields/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import MPENetworks from "singularitynet-platform-contracts/networks/MultiPartyEscrow";
 
 import { useStyles } from "./styles";
 import SNETTextfield from "shared/dist/components/SNETTextfield";
+import { useSelector } from "react-redux";
 
 const AdvancedFields = () => {
   const classes = useStyles();
+  const freeCallSignerAddress = useSelector(state => state.aiServiceDetails.freeCallSignerAddress);
   return (
     <div className={classes.advFilesContainer}>
       <Typography variant="subtitle1">Advanced Fields</Typography>
@@ -15,18 +18,16 @@ const AdvancedFields = () => {
       </Typography>
       <SNETTextfield
         icon
-        name="aiServicePrice"
-        // TODO value
-        // TODO onChange
-        label="Ai Service Price"
+        name="mpeAddress"
+        value={MPENetworks[process.env.REACT_APP_ETH_NETWORK].address}
+        label="MPE Address"
         description=" Lorem ipsum dolor sit amet, consectetur et mihi. Accusatores directam qui ut accusatoris. "
         disabled
       />
       <SNETTextfield
         icon
         name="aiServicePrice"
-        // TODO value
-        // TODO onChange
+        value={freeCallSignerAddress}
         label="Ai Service Price"
         description=" Lorem ipsum dolor sit amet, consectetur et mihi. Accusatores directam qui ut accusatoris. "
         disabled
