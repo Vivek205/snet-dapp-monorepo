ec830bf51890996d77fced4d784a3f860969b99e
Vivek205 <n.vin95@gmail.com>
2020-02-12 17:53:37 +0530

    AlertBox upgrade

--- packages/shared/src/components/AlertBox/index.js
+++ packages/shared/src/components/AlertBox/index.js
@@ -5,6 +5,8 @@ import { withStyles } from "@material-ui/styles";
 
 import { useStyles } from "./styles";
 import AlertLink from "./AlertLink";
+import AlertIcon from "./AlertIcon";
+import AlertHeader from "./AlertHeader";
 
 export const alertTypes = {
   ERROR: "error",
@@ -20,23 +22,21 @@ const backgroundColor = {
   info: alertTypes.INFO,
 };
 
-const AlertBox = ({ classes, message, type, link, icon: Icon, header }) => {
-  if (Icon) {
-    return (
-      <div className={clsx(classes.alertBoxContainer, classes[backgroundColor[type]])}>
-        <Icon />
-        <div className={classes.content}>
-          <span>{header}</span>
-          <p>{message}</p>
-        </div>
-      </div>
-    );
+const AlertBox = ({ classes, message, type, link, icon, header }) => {
+  if (!message) {
+    return null;
   }
 
   return (
-    <p className={clsx(classes.messageBox, classes[backgroundColor[type]])}>
-      {message} <AlertLink link={link} />
-    </p>
+    <div className={clsx(classes.alertBoxContainer, classes[backgroundColor[type]])}>
+      <AlertIcon icon={icon} />
+      <div className={classes.content}>
+        <AlertHeader header={header} />
+        <p>
+          {message} <AlertLink link={link} />
+        </p>
+      </div>
+    </div>
   );
 };
 
