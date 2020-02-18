f59b2e67ee618e2591f5746707aed2b8e09a4e4a
dasari-ananya <ananya.dasari@nirman.io>
2020-02-18 19:34:03 +0530

    tabs styling

--- packages/stake-dapp/src/Components/StakeTab/styles.js
+++ packages/stake-dapp/src/Components/StakeTab/styles.js
@@ -1,41 +1,36 @@
-export const useStyles = theme => ({
+export const useStyles = MUITheme => ({
   mainSection: {
-    padding: "48px 0 60px",
+    padding: "32px 0 60px",
     "& div": {
       "@media(max-width: 1024px)": { maxWidth: "100%" },
     },
     "@media(max-width: 1280px)": { paddingBottom: 30 },
     "@media(max-width: 1024px)": { flexDirection: "column" },
   },
-  servieMainContainer: {},
   tabsContainer: {
-    paddingLeft: 25,
     "@media(max-width: 1280px)": { paddingLeft: 0 },
   },
   header: {
     borderBottomWidth: 1,
     borderBottomStyle: "solid",
-    borderBottomColor: theme.palette.text.secodary,
+    borderBottomColor: "#e2e2e2",
     marginBottom: 16,
     position: "relative",
     backgroundColor: "transparent",
     boxShadow: "none",
     "& button": {
-      color: theme.palette.text.primary,
-      fontFamily: theme.palette.primary.main,
-      fontSize: 18,
+      color: MUITheme.palette.text.lightGrey,
+      fontFamily: MUITheme.typography.fontFamily,
+      fontSize: 20,
       textTransform: "capitalize",
-      lineHeight: "23px",
+      lineHeight: "25px",
       "&:nth-child(n+3)": {
         "@media(max-width:720px)": { display: "none" },
       },
     },
-    "& .MuiTab-textColorPrimary.Mui-selected": {
-      color: theme.palette.text.primary,
-      fontWeight: "bold",
-    },
+    "& .MuiTab-textColorPrimary.Mui-selected": { fontWeight: 600 },
     "& .PrivateTabIndicator-colorPrimary-289": {
-      backgroundColor: theme.palette.text.primary,
+      backgroundColor: MUITheme.palette.text.primary,
     },
   },
   tabDetailsContainer: {
