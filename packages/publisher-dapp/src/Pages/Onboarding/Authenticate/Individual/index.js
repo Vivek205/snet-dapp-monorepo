87e3f5eada2584c451ca5f6d88043dc7496266bf
dasari-ananya <ananya.dasari@nirman.io>
2020-02-25 13:07:46 +0530

    created and styled individual component

--- packages/publisher-dapp/src/Pages/Onboarding/Authenticate/Individual/index.js
+++ packages/publisher-dapp/src/Pages/Onboarding/Authenticate/Individual/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
 import SNETButton from "shared/dist/components/SNETButton";
+
+import { documentList } from "./content";
+import { useStyles } from "./styles";
 import { individualVerificationActions } from "../../../../Services/Redux/actionCreators/userActions";
 
 class Individual extends Component {
@@ -15,11 +22,35 @@ class Individual extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        Individual
-        <SNETButton onClick={this.handleVerify}>Verify via Jumio</SNETButton>
-      </div>
+      <Grid container className={classes.individualContainer}>
+        <Grid item sx={12} sm={12} md={12} lg={12} className={classes.box}>
+          <Typography variant="h6">Identity Verification Required</Typography>
+          <Grid item sx={12} sm={12} md={12} lg={12} className={classes.descriptionLogoSection}>
+            <Grid item sx={12} sm={12} md={8} lg={8} className={classes.description}>
+              <Typography>
+                You need to verify your identification. We use the secured third party service <span>Jumio </span>to
+                verifiy your identity. After you complete Jumio’s process, you will be redirected back to AI Publiser.
+              </Typography>
+            </Grid>
+            <Grid item sx={12} sm={12} md={4} lg={4} className={classes.jumioLogo}>
+              <img src="http://placehold.it/180x63" alt="Jumio" />
+            </Grid>
+          </Grid>
+          <Grid item sx={12} sm={12} md={12} lg={12} className={classes.docListSection}>
+            <Typography>Please prepare the following documents and information:</Typography>
+            <ul>
+              {documentList.map(item => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </Grid>
+          <Grid item sx={12} sm={12} md={12} lg={12} className={classes.btnContainer}>
+            <SNETButton children="process with jumio verification" color="primary" variant="contained" />
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
@@ -29,4 +60,4 @@ const mapDispatchToProps = dispatch => ({
   getVerificationStatus: () => dispatch(individualVerificationActions.getVerificationStatus()),
 });
 
-export default connect(null, mapDispatchToProps)(Individual);
+export default connect(null, mapDispatchToProps)(withStyles(useStyles)(Individual));
