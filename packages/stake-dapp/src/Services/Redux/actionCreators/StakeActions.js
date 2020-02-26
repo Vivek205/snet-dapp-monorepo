509d7041bc1ed257a7bb83e31dd1b5311aa27132
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2020-02-26 13:05:04 +0530

    Added initial API for create stake n code refactored

--- packages/stake-dapp/src/Services/Redux/actionCreators/StakeActions.js
+++ packages/stake-dapp/src/Services/Redux/actionCreators/StakeActions.js
@@ -6,6 +6,13 @@ import { APIEndpoints, APIPaths } from "../../AWS/APIEndpoints";
 import { initializeAPIOptions } from "../../../Utils/API";
 import { fetchAuthenticatedUser } from "./userActions/loginActions";
 
+export const UPDATE_ACTIVE_STAKE_WINDOW = "UPDATE_ACTIVE_STAKE_WINDOW";
+
+export const setActiveStakeWindowDetails = stakeWindowDetails => ({
+  type: UPDATE_ACTIVE_STAKE_WINDOW,
+  payload: stakeWindowDetails,
+});
+
 const fetchCurrentActiveStakeWindowAPI = metamaskDetails => async dispatch => {
   let staker = "0x0";
   const status = "OPEN";
@@ -23,13 +30,14 @@ const fetchCurrentActiveStakeWindowAPI = metamaskDetails => async dispatch => {
 export const fetchCurrentActiveStakeWindow = metamaskDetails => async dispatch => {
   try {
     //dispatch(loaderActions.startRequestLoader);
+
     const { data, error } = await dispatch(fetchCurrentActiveStakeWindowAPI(metamaskDetails));
-    //TODO - Check the error condition
-    if (error.code || !data) {
+    if (error.code) {
       throw new APIError(error.message);
     }
     //console.log("fetchCurrentActiveStakeWindow - ", data);
-    // TODO - Call dispatch Update the Redux Store with the values
+    const stakeWindowDetails = parseAndTransformStakeWindow(data[0]);
+    dispatch(setActiveStakeWindowDetails(stakeWindowDetails));
 
     //dispatch(loaderActions.stopRequestLoader);
   } catch (error) {
@@ -37,3 +45,44 @@ export const fetchCurrentActiveStakeWindow = metamaskDetails => async dispatch =
     throw error;
   }
 };
+
+const parseAndTransformStakeWindow = data => {
+  const stakeWindowDetails = {
+    stakeMapIndex: data.blockchain_id,
+    startPeriod: data.start_period,
+    submissionEndPeriod: data.submission_end_period,
+    approvalEndPeriod: data.approval_end_period,
+    requestWithdrawStartPeriod: data.request_withdraw_start_period,
+    endPeriod: data.end_period,
+    minStake: data.min_stake,
+    maxStake: data.max_stake,
+    windowMaxCap: data.window_max_cap,
+    openForExternal: data.open_for_external,
+    windowTotalStake: data.total_stake,
+    rewardAmount: data.reward_amount,
+    tokenOperator: data.token_operator,
+    totalStakers: 0,
+    totalStakedAmount: 0,
+    myStake: 0,
+  };
+
+  return stakeWindowDetails;
+};
+
+// TODO - Sample Structured returned from the API - TO BE Deleted from this file after the complete implementation:
+
+/* 
+blockchain_id: 100
+start_period: 1582542242
+submission_end_period: 1582742242
+approval_end_period: 1582842242
+request_withdraw_start_period: 1583142242
+end_period: 1583242242
+min_stake: 100000000
+max_stake: 1000000000000
+window_max_cap: 100000000000000
+open_for_external: true
+total_stake: 0
+reward_amount: 100000
+token_operator: "0x0"
+*/
---
+++ packages/stake-dapp/src/Services/Redux/reducers/StakeReducer.js
@@ -0,0 +1,38 @@
+import { stakeActions } from "../actionCreators";
+
+const InitialRequestDetails = {
+  activeStake: {
+    stakeMapIndex: 0,
+    startPeriod: 0,
+    submissionEndPeriod: 0,
+    approvalEndPeriod: 0,
+    requestWithdrawStartPeriod: 0,
+    endPeriod: 0,
+    minStake: 0,
+    maxStake: 0,
+    windowMaxCap: 0,
+    openForExternal: false,
+    windowTotalStake: 0,
+    rewardAmount: 0,
+    tokenOperator: "0x0",
+    totalStakers: 0,
+    totalStakedAmount: 0,
+    myStake: 0,
+  },
+  incubationStakes: [],
+  claimStakes: [],
+  myTransactions: [],
+};
+
+const stakeReducer = (state = InitialRequestDetails, action) => {
+  switch (action.type) {
+    case stakeActions.UPDATE_ACTIVE_STAKE_WINDOW: {
+      return { ...state, activeStake: action.payload };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default stakeReducer;
