3c5e67c27e3bf64e5e921679c2efbd50d669acc4
Sagarika <sagarika.katikam@nirman.io>
2020-02-26 18:35:53 +0530

    Alert box and removing validatation for optional feilds

--- packages/shared/src/components/AlertBox/index.js
+++ packages/shared/src/components/AlertBox/index.js
@@ -5,8 +5,6 @@ import { withStyles } from "@material-ui/styles";
 
 import { useStyles } from "./styles";
 import AlertLink from "./AlertLink";
-import AlertIcon from "./AlertIcon";
-import AlertHeader from "./AlertHeader";
 
 export const alertTypes = {
   ERROR: "error",
@@ -22,7 +20,7 @@ const backgroundColor = {
   info: alertTypes.INFO,
 };
 
-const AlertBox = ({ classes, message, type, link, children, icon, header }) => {
+const AlertBox = ({ classes, message, type, link, children }) => {
   if (children) {
     return <p className={clsx(classes.messageBox, classes[backgroundColor[type]])}>{children}</p>;
   }
@@ -33,19 +31,9 @@ const AlertBox = ({ classes, message, type, link, children, icon, header }) => {
         {message} <AlertLink link={link} />
       </p>
     );
+  } else {
+    return null;
   }
-
-  return (
-    <div className={clsx(classes.alertBoxContainer, classes[backgroundColor[type]])}>
-      <AlertIcon icon={icon} />
-      <div className={classes.content}>
-        <AlertHeader header={header} />
-        <p>
-          {message} <AlertLink link={link} />
-        </p>
-      </div>
-    </div>
-  );
 };
 
 AlertBox.propTypes = {
