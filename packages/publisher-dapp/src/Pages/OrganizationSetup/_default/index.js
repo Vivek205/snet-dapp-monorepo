83848ba8174d140bf9769fe7ca850e555a1c4b77
Vivek205 <n.vin95@gmail.com>
2020-02-26 11:45:28 +0530

    added uuid on org routes redirection

--- packages/publisher-dapp/src/Pages/OrganizationSetup/_default/index.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/_default/index.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
 import { OrganizationSetupRoutes } from "../OrganizationSetupRouter/Routes";
 
 const Default = ({ history }) => {
   const { onboardingStatus } = useSelector(state => state.user);
+  const { orgUuid } = useParams();
 
   useEffect(() => {
-    return history.push(OrganizationSetupRoutes.ORGANIZATION_PROFILE.path);
-  }, [history, onboardingStatus]);
+    return history.push(OrganizationSetupRoutes.ORGANIZATION_PROFILE.path.replace("orgUuid", orgUuid));
+  }, [history, onboardingStatus, orgUuid]);
 
   return <div />;
 };
