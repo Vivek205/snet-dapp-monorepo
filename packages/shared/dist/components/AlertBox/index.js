c236352c059ad431a3009a12f627de7d211d7a12
Vivek205 <n.vin95@gmail.com>
2020-02-27 18:16:37 +0530

    fixed imports

--- packages/shared/dist/components/AlertBox/index.js
+++ packages/shared/dist/components/AlertBox/index.js
@@ -17,6 +17,10 @@ var _styles2 = require("./styles");
 
 var _AlertLink = _interopRequireDefault(require("./AlertLink"));
 
+var _AlertIcon = _interopRequireDefault(require("./AlertIcon"));
+
+var _AlertHeader = _interopRequireDefault(require("./AlertHeader"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var alertTypes = {
@@ -51,11 +55,11 @@ var AlertBox = function AlertBox(_ref) {
   if (message) {
     return _react.default.createElement("div", {
       className: (0, _clsx.default)(classes.alertBoxContainer, classes[backgroundColor[type]])
-    }, _react.default.createElement(AlertIcon, {
+    }, _react.default.createElement(_AlertIcon.default, {
       icon: icon
     }), _react.default.createElement("div", {
       className: classes.content
-    }, _react.default.createElement(AlertHeader, {
+    }, _react.default.createElement(_AlertHeader.default, {
       header: header
     }), _react.default.createElement("p", null, message, " ", _react.default.createElement(_AlertLink.default, {
       link: link
