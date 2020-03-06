adeaea3dfd5490228698e94901cc06346f1d6cec
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2020-03-06 12:38:20 +0530

    Refined Timer

--- packages/stake-dapp/src/Components/CreateStake/index.js
+++ packages/stake-dapp/src/Components/CreateStake/index.js
@@ -26,12 +26,6 @@ const stateSelector = state => ({
   metamaskDetails: state.metamaskReducer.metamaskDetails,
 });
 
-// TODO - Delete this element
-const testStake = {
-  startPeriod: 1583476850,
-  endPeriod: 1583476950,
-};
-
 const CreateStake = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -79,7 +73,6 @@ const CreateStake = () => {
     return (
       <Grid container>
         <Grid item xs={12} sm={12} md={4} lg={4}>
-          <SessionTime stakeDetails={testStake} />
           <AccountBalance />
         </Grid>
         <Grid item xs={12} sm={12} md={8} lg={8} className={classes.rightSideSection}>
@@ -96,7 +89,7 @@ const CreateStake = () => {
   return (
     <Grid container>
       <Grid item xs={12} sm={12} md={4} lg={4}>
-        <SessionTime stakeDetails={testStake} />
+        <SessionTime stakeDetails={activeStake} />
         <AccountBalance />
       </Grid>
       <Grid item xs={12} sm={12} md={8} lg={8} className={classes.rightSideSection}>
