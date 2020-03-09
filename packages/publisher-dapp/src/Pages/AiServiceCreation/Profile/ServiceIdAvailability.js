be8e0fe9f1facd2bd8403370614706a6bc698f4d
Vivek205 <n.vin95@gmail.com>
2020-03-09 15:00:23 +0530

    service id availability

--- packages/publisher-dapp/src/Pages/AiServiceCreation/Profile/ServiceIdAvailability.js
+++ packages/publisher-dapp/src/Pages/AiServiceCreation/Profile/ServiceIdAvailability.js
@@ -5,21 +5,31 @@ import AlertText from "shared/dist/components/AlertText";
 import { alertTypes } from "shared/dist/components/AlertBox";
 import { serviceIdAvailability } from "../constant";
 
-const ServiceIdAvailability = ({ classes, id, availability, loading }) => {
+const ServiceIdAvailability = ({ classes, serviceDetails, id, availability, loading }) => {
+  if (!Boolean(serviceDetails.newId) && !Boolean(serviceDetails.id)) {
+    return null;
+  }
   if (loading) {
     return (
       <div className={classes.alertTextContainer}>
-        <AlertText type={alertTypes.INFO} message="validating..." />;
+        <AlertText type={alertTypes.INFO} message="validating..." />
       </div>
     );
   }
 
+  const type =
+    !Boolean(serviceDetails.newId) || availability === serviceIdAvailability.AVAILABLE
+      ? alertTypes.SUCCESS
+      : alertTypes.ERROR;
+
+  const message =
+    !isEmpty(id) && Boolean(availability) && serviceDetails.newId !== serviceDetails.id
+      ? `Service Id is ${availability}`
+      : "";
+
   return (
     <div className={classes.alertTextContainer}>
-      <AlertText
-        type={availability === serviceIdAvailability.AVAILABLE ? alertTypes.SUCCESS : alertTypes.ERROR}
-        message={!isEmpty(id) ? `Service Id is ${availability}` : ""}
-      />
+      <AlertText type={type} message={message} />
     </div>
   );
 };
