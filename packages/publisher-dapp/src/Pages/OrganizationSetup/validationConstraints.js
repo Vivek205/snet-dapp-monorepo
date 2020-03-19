9466958759fdfa43eb93bf235a5661acc99f6708
Vivek205 <n.vin95@gmail.com>
2020-03-19 17:54:48 +0530

    organization validation

--- packages/publisher-dapp/src/Pages/OrganizationSetup/validationConstraints.js
+++ packages/publisher-dapp/src/Pages/OrganizationSetup/validationConstraints.js
@@ -7,21 +7,33 @@ export const submitOrganizationCostraints = {
   shortDescription: { presence: { allowEmpty: false } },
   longDescription: { presence: { allowEmpty: false } },
 
-  "assets.heroImage.raw": {
-    presence: { allowEmpty: false, message: "Please upload a hero image for the organization", fullMessages: false },
-  },
-  "assets.heroImage.fileType": {
-    presence: { allowEmpty: false, message: "Please upload a hero image for the organization", fullMessages: false },
+  "assets.heroImage.url": {
+    presence: {
+      allowEmpty: false,
+      message: "^Please upload an image for the organization",
+      fullMessages: false,
+    },
   },
   groups: {
     array: {
-      name: { presence: { allowEmpty: false } },
-      id: { presence: { allowEmpty: false } },
-      paymentAddress: { presence: { allowEmpty: false }, equality: validBlockChainAdressEquality("paymentAddress") },
-      "paymentConfig.paymentExpirationThreshold": { presence: { allowEmpty: false } },
-      "paymentConfig.paymentChannelStorageType": { presence: { allowEmpty: false } },
-      "paymentConfig.paymentChannelStorageClient.connectionTimeout": { presence: { allowEmpty: false } },
-      "paymentConfig.paymentChannelStorageClient.requestTimeout": { presence: { allowEmpty: false } },
+      name: { presence: { allowEmpty: false, message: "^Please enter group details" } },
+      id: { presence: { allowEmpty: false, message: "^Please enter group details" } },
+      paymentAddress: {
+        presence: { allowEmpty: false },
+        equality: validBlockChainAdressEquality("paymentAddress"),
+      },
+      "paymentConfig.paymentExpirationThreshold": {
+        presence: { allowEmpty: false, message: "^Please enter payment expiration threshold" },
+      },
+      "paymentConfig.paymentChannelStorageType": {
+        presence: { allowEmpty: false, message: "^Please enter payment channel storage type" },
+      },
+      "paymentConfig.paymentChannelStorageClient.connectionTimeout": {
+        presence: { allowEmpty: false, message: "^Please enter connection timeout" },
+      },
+      "paymentConfig.paymentChannelStorageClient.requestTimeout": {
+        presence: { allowEmpty: false, message: "^Please enter request timeout" },
+      },
     },
   },
 };
