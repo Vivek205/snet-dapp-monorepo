bfbbc293e61085b10eb00d788bd4a094e3265d6e
Vivek205 <n.vin95@gmail.com>
2020-03-21 19:32:41 +0530

    org verification status - reject reason

--- packages/publisher-dapp/src/Services/Redux/actionCreators/userActions/index.js
+++ packages/publisher-dapp/src/Services/Redux/actionCreators/userActions/index.js
@@ -3,5 +3,13 @@ import * as signupActions from "./signupActions";
 import * as onboardingActions from "./onboardingActions";
 import * as preferenceActions from "./preferencesActions";
 import * as individualVerificationActions from "./individualVerificationActions";
+import * as orgVerificationActions from "./orgVerificationActions";
 
-export { loginActions, signupActions, onboardingActions, preferenceActions, individualVerificationActions };
+export {
+  loginActions,
+  signupActions,
+  onboardingActions,
+  preferenceActions,
+  individualVerificationActions,
+  orgVerificationActions,
+};
