61aedab37e40ae8581d3caa0ef2c5124b1186440
SagarikaKatikam <61489562+SagarikaKatikam@users.noreply.github.com>
2020-04-08 13:11:48 +0530

    Validator changes to handle an array of errors

--- packages/shared/src/utils/validator.js
+++ packages/shared/src/utils/validator.js
@@ -2,28 +2,28 @@ import validate from "validate.js";
 
 const validator = validate;
 
-const hasLowerCase = (value, options, key, attributes) => {
+const hasLowerCase = (value, options) => {
   if (/[a-z]/.test(value)) {
     return;
   }
   return options.message || "must contain a lowercase character";
 };
 
-const hasUpperCase = (value, options, key, attributes) => {
+const hasUpperCase = (value, options) => {
   if (/[A-Z]/.test(value)) {
     return;
   }
   return options.message || "must contain an uppercase character";
 };
 
-const hasNumber = (value, options, key, attributes) => {
+const hasNumber = (value, options) => {
   if (/[0-9]/.test(value)) {
     return;
   }
   return options.message || "must contain a number";
 };
 
-const hasAWSPasswordSplChar = (value, options, key, attributes) => {
+const hasAWSPasswordSplChar = (value, options) => {
   // eslint-disable-next-line no-useless-escape
   if (/[\^\$\*\.\[\]\{\}\(\)\?\-\"\!\@\#\%\&\/\,\>\<\'\:\;\|\_\~\`]/.test(value)) {
     return;
@@ -35,12 +35,12 @@ const array = (arrayItems, itemConstraints, key) => {
   if (!validate.isArray(arrayItems)) {
     return `${key} is not a valid array`;
   }
-  const arrayItemErrors = arrayItems.reduce((errors, item, index) => {
+  const arrayItemErrors = arrayItems.reduce((errors, item) => {
     const error = validate(item, itemConstraints);
     if (error) errors.push(...error);
     return errors;
   }, []);
-  return validate.isEmpty(arrayItemErrors) ? null : `^${arrayItemErrors[0]}`;
+  return validate.isEmpty(arrayItemErrors) ? null : `^${arrayItemErrors}`;
 };
 
 validator.validators = {
