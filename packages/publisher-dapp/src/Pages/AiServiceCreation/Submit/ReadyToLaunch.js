5efaa791ca2e7542070eb953e556ad86de706a8f
Vivek205 <n.vin95@gmail.com>
2020-04-14 19:41:49 +0530

    VerificationApproved Image

--- packages/publisher-dapp/src/Pages/AiServiceCreation/Submit/ReadyToLaunch.js
+++ packages/publisher-dapp/src/Pages/AiServiceCreation/Submit/ReadyToLaunch.js
@@ -1,14 +1,36 @@
 import React, { Fragment } from "react";
 import SNETStatusBanner, { statusTitleType } from "shared/dist/components/SNETStatusBanner";
 import AlertBox from "shared/dist/components/AlertBox";
+import VerificationApprovedImage from "shared/dist/assets/images/VerificationApproved.png";
+import Typography from "@material-ui/core/Typography";
 
 const ReadyToLaunch = ({ handlePublish, handleBackToDashboard, alert }) => {
   return (
     <Fragment>
       <SNETStatusBanner
-        title="Your AI service review is in progress…"
-        img="http://placehold.it/302x242"
-        description="This review may take a day or two.  After the review you will be notified if your AI service is accepted or if some inputs need to be refined."
+        title="Ready to Launch"
+        img={VerificationApprovedImage}
+        description={
+          <Fragment>
+            <Typography> Please proceed to launch to complete the final step.</Typography>
+            <ul>
+              <li>
+                <Typography>
+                  The final launch will require you to be logged into your Metamask with some ETH available to activate
+                  the service.
+                </Typography>
+              </li>
+              <li>
+                <Typography>Only the owner of the organization can launch an update.</Typography>
+              </li>
+              <li>
+                <Typography>
+                  Once you launch the update, it will take some for your changes to be reflected on AI Marketplace.
+                </Typography>
+              </li>
+            </ul>
+          </Fragment>
+        }
         actions={[
           { children: "Launch Service", variant: "contained", color: "primary", onClick: handlePublish },
           { children: "back to dashboard", variant: "outlined", color: "primary", onClick: handleBackToDashboard },
