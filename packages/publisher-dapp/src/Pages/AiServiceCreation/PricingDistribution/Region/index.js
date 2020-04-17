f90bd11b423b8d3e7d7845f3e137e12a26ebc7c3
SagarikaKatikam <61489562+SagarikaKatikam@users.noreply.github.com>
2020-04-17 12:06:25 +0530

    setting message

--- packages/publisher-dapp/src/Pages/AiServiceCreation/PricingDistribution/Region/index.js
+++ packages/publisher-dapp/src/Pages/AiServiceCreation/PricingDistribution/Region/index.js
@@ -254,7 +254,7 @@ const Region = ({ changeGroups, serviceGroups }) => {
               }}
             />
           </Grid>
-          <AlertBox type={alert.type} children={alert.children} />
+          <AlertBox type={alert.type} children={alert.message} />
 
           <Grid item xs={12} sm={12} md={12} lg={12} className={classes.addedEndpointsContainer}>
             <div className={classes.infoIconContainer}>
