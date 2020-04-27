117d9a120313405b611345150d8de5409215cdad
SagarikaKatikam <61489562+SagarikaKatikam@users.noreply.github.com>
2020-04-27 12:48:10 +0530

    Revert "testendpoint validation"
    This reverts commit 531eb905d2feabc209d065e87421b4c517f1815e.

--- packages/publisher-dapp/src/Pages/AiServiceCreation/Submit/ValidateConfig.js
+++ packages/publisher-dapp/src/Pages/AiServiceCreation/Submit/ValidateConfig.js
@@ -20,7 +20,7 @@ const ValidateConfig = props => {
     testEndpointAlert,
   } = props;
   const shouldValidateDaemonEnabled = () => {
-    const error = validator.single(testEndPoint, submitServiceConstraints.groups.array.testEndpoints);
+    const error = validator.single(testEndPoint, submitServiceConstraints.testEndpoints);
     return Boolean(error);
   };
 
