01975a86b3cd3018c137134eb7dc5e570c7b6476
Vivek205 <n.vin95@gmail.com>
2020-04-28 10:20:24 +0530

    added router for login story

--- packages/shared/src/components/SNETLogin/index.stories.js
+++ packages/shared/src/components/SNETLogin/index.stories.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import withLiveEditScope from "storybook-addon-react-live-edit/dist/withLiveEditScope";
+import { BrowserRouter as ReactRouter } from "react-router-dom";
 
 import SNETLogin from "./";
 
 storiesOf("SNETLogin", module)
   .addParameters({ props: { propTables: [SNETLogin] } })
-  .addDecorator(withLiveEditScope({ React, SNETLogin }))
+  .addDecorator(withLiveEditScope({ React, SNETLogin, ReactRouter }))
   .addLiveSource(
     "live source",
-    `return <SNETLogin title="SingularityNET" forgotPasswordLink="#" 
-              loginError="Username Password Error" onSubmit={console.log}/>`
+    `return   <ReactRouter>
+                <SNETLogin title="SingularityNET" forgotPasswordLink="#" 
+                loginError="Username Password Error" onSubmit={console.log}/>  
+              </ReactRouter>`
   );
