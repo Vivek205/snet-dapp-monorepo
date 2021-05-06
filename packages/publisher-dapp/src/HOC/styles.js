5091a073c7e1b1ee18ce83931c961ebdb6d30c77
dasari-ananya <ananya.dasari@nirman.io>
2021-05-06 15:26:47 +0530

    makestyles added to hoc styles

--- packages/publisher-dapp/src/HOC/styles.js
+++ packages/publisher-dapp/src/HOC/styles.js
@@ -1,4 +1,6 @@
-export const useStyles = () => ({
+import { makeStyles } from "@material-ui/styles";
+
+export const useStyles = makeStyles({
   increaseTopSpace: { marginTop: 105 },
   addMarginTopToVerticalTab: {
     "& .MuiPaper-root": { top: 111 },
