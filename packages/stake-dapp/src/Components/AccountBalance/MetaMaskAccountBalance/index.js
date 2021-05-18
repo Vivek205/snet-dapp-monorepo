47936b4bb82347edafc699914658f1a08fa3dce4
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2021-05-18 16:32:36 +0530

    updated event text

--- packages/stake-dapp/src/Components/AccountBalance/MetaMaskAccountBalance/index.js
+++ packages/stake-dapp/src/Components/AccountBalance/MetaMaskAccountBalance/index.js
@@ -162,7 +162,7 @@ class MetaMaskAccountBalance extends Component {
                 <InfoIcon />
                 <p>
                   Number of AGIX tokens that you have staked across all sessions. This includes pending and active
-                  staking amounts.
+                  staking amounts along with rewards.
                 </p>
               </div>
               <span>Total Staked</span>
