9954ed8aa3b38dc18913afe20e53dbb2736b3a5d
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2021-05-21 20:36:21 +0530

    Revert "Development staking"

--- packages/stake-dapp/src/HOC/withLightHeaderAndFooter.js
+++ packages/stake-dapp/src/HOC/withLightHeaderAndFooter.js
@@ -7,30 +7,16 @@ import SNETFooter from "shared/dist/components/SNETFooter";
 import { FooterData } from "./footerContent";
 import Header from "../Components/Header";
 
-import { useStyles } from "./styles";
-import { localStorageKeys, useLocalStorage } from "shared/dist/hooks/useLocalStorage";
-
 const withLightHeaderAndFooter = Component => {
-  return props => {
-    const classes = useStyles();
-    const [showUpdateNotification, setShowUpdateNotificationBar] = useLocalStorage(
-      localStorageKeys.SHOW_PHASE2_NOTIFICATION,
-      true
-    );
-
-    const onUpdateCloseClick = () => {
-      setShowUpdateNotificationBar(false);
-    };
-
-    return (
-      <Fragment>
-        <Header showNotification={showUpdateNotification} onCloseClick={onUpdateCloseClick} />
-        <Box mt={8} className={showUpdateNotification ? classes.increaseTopSpace : null}>
-          <Component {...props} />
-        </Box>
-        <SNETFooter data={FooterData} />
-      </Fragment>
-    );
-  };
+  return props => (
+    <Fragment>
+      <Header />
+      <Box mt={8}>
+        <Component {...props} />
+      </Box>
+      <SNETFooter data={FooterData} />
+    </Fragment>
+  );
 };
+
 export default withLightHeaderAndFooter;
