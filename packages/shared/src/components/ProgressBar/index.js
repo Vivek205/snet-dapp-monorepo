eb9002a58ee38872612f19f7ab5284d6c9b7de72
Rajeev <rajeev.radhakrishnan@singularitynet.io>
2021-06-24 13:39:18 +0530

    Revert "Merge branch 'development-publisher' of https://github.com/rajeevbbqq/snet-dapp-monorepo into development-publisher"
    This reverts commit 3c10577f09540bb9b205d64b3fe1b636ef225f1f, reversing
    changes made to ff1a7962d24368125dc3deb44deaadbab800397e.

---
+++ packages/shared/src/components/ProgressBar/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
+
+import ProgressSection, { ProgressStatusList } from "./ProgressSection";
+import { useStyles } from "./styles";
+
+const ProgressBar = ({ classes, activeSection, progressText, onSectionClick, progressStatus }) => {
+  const computeProgressStatus = (progressNumber, activeSection) => {
+    if (progressNumber < Number(activeSection)) {
+      return ProgressStatusList.COMPLETED;
+    }
+    if (progressNumber === Number(activeSection)) {
+      return ProgressStatusList.ACTIVE;
+    }
+    if (progressNumber > Number(activeSection)) {
+      return ProgressStatusList.IDLE;
+    }
+  };
+
+  const handleProgressBarStatus = (progressNumber, activeSection) => {
+    if (progressStatus[progressNumber]) {
+      return progressStatus[progressNumber].toLowerCase();
+    }
+    return computeProgressStatus(progressNumber, activeSection);
+  };
+
+  return (
+    <div className={classes.tabsContainer}>
+      <ul>
+        {progressText.map((text, index) => (
+          <ProgressSection
+            progressNumber={index + 1}
+            progressText={text}
+            progressStatus={handleProgressBarStatus(index + 1, activeSection)}
+            key={text}
+            onSectionClick={onSectionClick}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ProgressBar.propTypes = {
+  activeSection: PropTypes.number,
+  progressText: PropTypes.arrayOf(PropTypes.string),
+  onSectionClick: PropTypes.func,
+  progressStatus: PropTypes.object,
+};
+
+ProgressBar.defaultProps = {
+  progressStatus: {},
+};
+
+export default withStyles(useStyles)(ProgressBar);
